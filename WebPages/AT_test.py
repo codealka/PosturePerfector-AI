0de4424e42a893371b35670e64acbb088d034df4
codealka <Alkacoding@gmail.com>
2022-04-15 18:32:30 +0100

    trying option menu deployment

--- WebPages/AT_test.py
+++ WebPages/AT_test.py
@@ -6,7 +6,7 @@ def app():
 
     @st.cache(allow_output_mutation=True)
     def load_camera():
-        CAMERA_FLAG = 0
+        CAMERA_FLAG = 1
         camera = cv2.VideoCapture(CAMERA_FLAG)
         return camera
 
