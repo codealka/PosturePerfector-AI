bcdc4228fa86aeacac5e5ee9904c8b37719da42c
codealka <Alkacoding@gmail.com>
2022-04-15 18:32:06 +0100

    trying option menu deployment

--- WebPages/AT_test.py
+++ WebPages/AT_test.py
@@ -4,29 +4,10 @@ import cv2
 
 def app():
 
-    st.title("Postural Imaging System for Back Pain Sufferers")
+    @st.cache(allow_output_mutation=True)
+    def load_camera():
+        CAMERA_FLAG = 0
+        camera = cv2.VideoCapture(CAMERA_FLAG)
+        return camera
 
-    option = st.selectbox(
-         'Video input medium',
-         ('Device Native Camera', 'Additional Webcam'))
-
-    st.write('You selected:', option)
-
-    if option == 'Device Native Camera':
-            camera = 0 # camera used ( 0 = native camera , 1 = added webcam)
-    else:
-            camera = 1
-
-    vf = cv2.VideoCapture(1)
-
-    stframe = st.empty()
-
-    while vf.isOpened():
-        ret, frame = vf.read()
-        # if frame is read correctly ret is True
-        if not ret:
-            st.write("not")
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        stframe.image(gray)
+    camera = load_camera()
